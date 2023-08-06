caf79bc6bea081c683ab34150520aeebaa2fe108
Katie Patterson <kpatterson67@gmail.com>
2023-08-06 13:23:42 -0500

    fix

--- new_digs_django/new_digs_app/apps.py
+++ new_digs_django/new_digs_app/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class NewDigsAppConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "new_digs_django_app"
+    name = "new_digs_app"
--- new_digs_django/settings.py
+++ new_digs_django/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
-    "new_digs_django_app.apps.NewDigsAppConfig",
+    "new_digs_app.apps.NewDigsAppConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
